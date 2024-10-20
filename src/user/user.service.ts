212c6d7a450abb6a64d20a109476f18b36a1ee9b
khaled <khalidrouissi7@gmail.com>
2024-10-20 19:12:06 +0100

    config ci cd

---
+++ src/user/user.service.ts
@@ -0,0 +1,145 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { RegisterUserDto } from './dto/register-user.dto';
+import * as firebaseAdmin from 'firebase-admin';
+import axios from 'axios';
+import { LoginDto } from './dto/login.dto';
+
+@Injectable()
+export class UserService {
+
+  async registerUser(registerUser: RegisterUserDto) {
+    console.log(registerUser);
+    try {
+      const userRecord = await firebaseAdmin.auth().createUser({
+        displayName: registerUser.firstName,
+        email: registerUser.email,
+        password: registerUser.password,
+      });
+      console.log('User Record:', userRecord);
+      return userRecord;
+    } catch (error) {
+      console.error('Error creating user:', error);
+      throw new Error('User registration failed'); 
+    }
+  }
+
+  async loginUser(payload: LoginDto) {
+    const { email, password } = payload;
+    try {
+      const { idToken, refreshToken, expiresIn } =
+        await this.signInWithEmailAndPassword(email, password);
+      return { idToken, refreshToken, expiresIn };
+    } catch (error: any) {
+      if (error.message.includes('EMAIL_NOT_FOUND')) {
+        throw new Error('User not found.');
+      } else if (error.message.includes('INVALID_PASSWORD')) {
+        throw new Error('Invalid password.');
+      } else {
+        throw new Error(error.message);
+      }
+    }
+
+  }
+  private async signInWithEmailAndPassword(email: string, password: string) {
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${process.env.APIKEY}`;
+    const response = await this.sendPostRequest(url, {
+      email,
+      password,
+      returnSecureToken: true,
+    });
+    console.log('Firebase login response:', response);
+    return response;
+  }
+  private async sendPostRequest(url: string, data: any) {
+    try {
+      const response = await axios.post(url, data, {
+        headers: { 'Content-Type': 'application/json' },
+      });
+      return response.data;
+    } catch (error) {
+      console.log('error', error);
+    }
+  }
+
+  async validateRequest(req): Promise<boolean> {
+    const authHeader = req.headers['authorization'];
+    if (!authHeader) {
+      console.log('Authorization header not provided.');
+      return false;
+    }
+    
+    const [bearer, token] = authHeader.split(' ');
+    if (bearer !== 'Bearer' || !token) {
+      console.log('Invalid authorization format. Expected "Bearer <token>".');
+      return false;
+    }
+    
+    try {
+      const decodedToken = await firebaseAdmin.auth().verifyIdToken(token);
+      console.log('Decoded Token:', decodedToken);
+      return true;
+    } catch (error: any) { // Change here
+      if (error.code === 'auth/id-token-expired') {
+        console.error('Token has expired.');
+      } else if (error.code === 'auth/invalid-id-token') {
+        console.error('Invalid ID token provided.');
+      } else {
+        console.error('Error verifying token:', error);
+      }
+      return false;
+    }
+  }
+
+  async refreshAuthToken(refreshToken: string) {
+    try {
+      const {
+        id_token: idToken,
+        refresh_token: newRefreshToken,
+        expires_in: expiresIn,
+      } = await this.sendRefreshAuthTokenRequest(refreshToken);
+      return {
+        idToken,
+        refreshToken: newRefreshToken,
+        expiresIn,
+      };
+    } catch (error: any) {
+      if (error.message.includes('INVALID_REFRESH_TOKEN')) {
+        throw new Error(`Invalid refresh token: ${refreshToken}.`);
+      } else {
+        throw new Error('Failed to refresh token');
+      }
+    }
+  }
+  private async sendRefreshAuthTokenRequest(refreshToken: string) {
+    const url = `https://securetoken.googleapis.com/v1/token?key=${process.env.APIKEY}`;
+    const payload = {
+      grant_type: 'refresh_token',
+      refresh_token: refreshToken,
+    };
+    return await this.sendPostRequest(url, payload);
+  }
+  
+
+  
+  create(createUserDto: CreateUserDto) {
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all user`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
